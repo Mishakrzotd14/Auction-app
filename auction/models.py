2ddcf6ceb284943e53b6350b882422916c147baa
MKurziankou <MKurzenkov.otd14@gmail.com>
2024-01-25 17:19:06 +0300

    "Add apps (item, lot, auction) and define models"

---
+++ auction/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+AUCTION_STATUS_CHOICES = [
+    ('PENDING', 'Pending'),
+    ('IN_PROGRESS', 'In Progress'),
+    ('CLOSED', 'Closed'),
+]
+
+
+class Auction(models.Model):
+    opening_date = models.DateTimeField()
+    closing_date = models.DateTimeField()
+    auction_status = models.CharField(max_length=20, choices=AUCTION_STATUS_CHOICES)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        abstract = True
+
+
+class EnglishAuction(Auction):
+    opening_price = models.DecimalField(max_digits=10, decimal_places=2)
+    buy_it_now_price = models.DecimalField(max_digits=10, decimal_places=2)
+    reserve_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class DutchAuction(Auction):
+    start_price = models.DecimalField(max_digits=10, decimal_places=2)
+    end_price = models.DecimalField(max_digits=10, decimal_places=2)
+    frequency = models.IntegerField()
---
+++ lot/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from item.models import Item
+from auction.models import Auction
+
+
+class Lot(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
