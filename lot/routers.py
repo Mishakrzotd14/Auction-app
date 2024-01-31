17803992a5cb80794e9d49f22382d8fdf6662b66
MKurziankou <MKurzenkov.otd14@gmail.com>
2024-01-31 09:36:41 +0300

    Deleted routers.py in the lot

--- lot/routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from lot.views import LotListView
-
-router = routers.SimpleRouter()
-
-router.register('lot', LotListView, basename='lot')
-urlpatterns = router.urls
