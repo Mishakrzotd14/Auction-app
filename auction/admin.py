29c457f2edda0e48f8b14fb4de7734c4483ae032
MKurziankou <MKurzenkov.otd14@gmail.com>
2024-01-31 17:34:51 +0300

    Add pagination, ordering for Lots and filtering

--- auction/admin.py
+++ auction/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from auction.models import EnglishAuction, DutchAuction
+from lot.models import Lot
+
+
+class LotInline(admin.TabularInline):
+    model = Lot
+
+
+@admin.register(EnglishAuction)
+class EnglishAuctionAdmin(admin.ModelAdmin):
+    inlines = [LotInline]
+
+
+@admin.register(DutchAuction)
+class DutchAuctionAdmin(admin.ModelAdmin):
+    inlines = [LotInline]
--- lot/views.py
+++ lot/views.py
@@ -1,11 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 from lot.models import Lot
 from lot.serializers import LotSerializer
+
+
+class LotsLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 5
+    max_limit = 10
 
 
 class LotListView(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Lot.objects.all()
     serializer_class = LotSerializer
+    pagination_class = LotsLimitOffsetPagination
+    ordering_fields = ['-closing_date', 'base_price']
+    ordering = ['-closing_date']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['auction__auction_status', ]
