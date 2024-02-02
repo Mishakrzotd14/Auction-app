c0782f931318c0a54d1556c23094caa8ff87a108
Mishakrzotd14 <120625455+Mishakrzotd14@users.noreply.github.com>
2024-02-02 09:26:28 +0300

    Add auth: Token and JWT authentication (#8)
    * Add auth: Token and JWT authentication
    
    * fixed config/urls.py
    
    * fixed config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -99,4 +99,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
 }
