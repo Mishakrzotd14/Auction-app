b5f6d2e0328bedd3e469fef6fe2f51d72574e0e5
Mishakrzotd14 <120625455+Mishakrzotd14@users.noreply.github.com>
2024-02-26 16:58:14 +0300

    Add English auction logic (#10)
    * Add Offer model and validators for make_offer
    
    * Update file and create make_offer_buy_it_now
    
    * Update files
    
    * Update lot/urls
    
    ---------
    
    Co-authored-by: Misha <mishakrz@tut.by>

--- lot/admin.py
+++ lot/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from lot.models import Lot
+from lot.models import Lot, Offer
 
 
 class LotInline(admin.TabularInline):
     model = Lot
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ("user", "lot", "price", "date_creation",)
